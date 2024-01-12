0a5567f51a9427f0579cbb313890d49bb406e9f2
JoyRume <joy.rume@student.moringaschool.com>
2024-01-12 16:51:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+//return word===word.split('').reverse().join('')
+return word === word.split('').reverse().join('')
+ }
+
 
 /* 
   Add your pseudocode here
